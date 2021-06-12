697d54c54b45fc05332e6228459cc47ebe9f8987
Tuija <tuija.sonkkila@aalto.fi>
2021-06-12 22:15:16 +0300

    Changed rgbif call, added leaflet popup

--- preparedata.R
+++ preparedata.R
@@ -4,34 +4,25 @@ library(rnaturalearth)
 library(sf)
 library(leaflet)
 
-# Asian elephant subspecies
+# Asian elephant
 #
-# rgbif code by https://github.com/AChase44/FISH-504
-species <- occ_search(scientificName = "Elephas maximus indicus", limit = 1000)
+# rgbif code snippet by https://github.com/AChase44/FISH-504
+species <- occ_search(scientificName = "Elephas maximus", limit = 5000)
 species_1 <- as.data.frame(species$data)
-el_indicus <- species_1[,c("scientificName","stateProvince", "year", "recordedBy", "references", "decimalLatitude","decimalLongitude")]
-
-species <- occ_search(scientificName = "Elephas maximus maximus", limit = 1000)
-species_1 <- as.data.frame(species$data)
-el_maximus <- species_1[,c("scientificName","stateProvince", "year", "recordedBy", "references", "decimalLatitude","decimalLongitude")]
-
-species <- occ_search(scientificName = "Elephas maximus sumatranus", limit = 1000)
-species_1 <- as.data.frame(species$data)
-el_sumatranus <- species_1[,c("scientificName","stateProvince", "year", "recordedBy", "references", "decimalLatitude","decimalLongitude")]
-
-el <- rbind(el_indicus, el_maximus, el_sumatranus)
+el <- species_1[,c("acceptedScientificName","stateProvince", "year", "recordedBy", "basisOfRecord", "references", "decimalLatitude","decimalLongitude")]
+write_rds(el, "el.RDS")
 
 el <- el %>% 
-  filter(decimalLongitude > 60) %>% # few bad data points
+  filter(decimalLongitude > 60, 
+         acceptedScientificName != "BOLD:AAF0248") %>% # few bad data points
   mutate(stateProvince = ifelse(is.na(stateProvince), "-", stateProvince),
          year = ifelse(is.na(year), "-", year),
          recordedBy = ifelse(is.na(recordedBy), "-", recordedBy),
-         references = ifelse(is.na(references), "-", references))
+         references = ifelse(is.na(references), "-", 
+                             ifelse(grepl("https?://", references), paste0("<a href='", references, "'>Link</a>"), 
+                                    references)))
 
-write_rds(el, "el.RDS")
-
-# el <- readRDS("el.RDS")
-
+# Plot
 asia <- ne_countries(continent = "asia", scale = "medium", returnclass = "sf")
 
 plot <- ggplot() +
@@ -39,29 +30,29 @@ plot <- ggplot() +
   geom_point(data = el, aes(x = decimalLongitude, y = decimalLatitude, fill = scientificName),
              pch = 21, size = 2.5, alpha = 0.4) +
   scale_fill_viridis_d(option = "inferno") +
-  guides(fill = guide_legend(title = "Subspecies")) +
+  guides(fill = guide_legend(title = "")) +
   theme_void()
 
-elCol <- colorFactor(palette = 'RdYlGn', el$scientificName)
+# Map
+elCol <- colorFactor(palette = 'RdYlGn', el$basisOfRecord)
 
 leaflet() %>%
   addTiles() %>% 
   setView(lat = mean(el$decimalLatitude), 
           lng = mean(el$decimalLongitude), 
           zoom = 3) %>% 
-  # addPolygons(data = asia,
-  #             fillColor = "snow",
-  #             stroke = TRUE,
-  #             color = "grey",
-  #             weight = 1) %>%
   addCircleMarkers(data = el,
                    lat = ~decimalLatitude,
                    lng = ~decimalLongitude,
-                   popup = ~paste("<b>Scientific name:</b> ", scientificName, "<br/>", 
+                   popup = ~paste("<b>Basis of record:</b> ", basisOfRecord, "<br/>",
+                                  "<b>Scientific name:</b> ", acceptedScientificName, "<br/>", 
                                   "<b>State/Province:</b> ", stateProvince, "<br/>",
                                   "<b>Year:</b> ", year, "<br/>", 
                                   "<b>Recorded by:</b> ", recordedBy, "<br/>", 
                                   "<b>References:</b> ", references),
-                   color = ~elCol(scientificName),
+                   color = ~elCol(basisOfRecord),
                    weight = 2, 
-                   opacity = 0.4) 
+                   opacity = 0.4) %>% 
+  addLegend(pal = elCol, values = el$basisOfRecord, title = "Basis of record",
+            position = "bottomright")
+
